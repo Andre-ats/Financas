32dadf228e86c749ed78ea18852514310a40dab9
Andre <andre.ats.profissional@gmail.com>
2024-12-15 20:50:07 -0300

    Criacao de Receita e gastos

--- Domain/Entity/Financas/Gasto.cs
+++ Domain/Entity/Financas/Gasto.cs
@@ -2,6 +2,24 @@ namespace Financas.Entity.Financas;
 
 public class Gasto : IEntity
 {
-    
-    
+    public string TituloGasto { get; protected set; } = String.Empty;
+    public string? DescricaoGasto { get; protected set; }
+    public string? Estabelecimento { get; protected set; }
+    public long GastoDinheiro { get; protected set; }
+    public DateTime DataGasto { get; protected set; }
+
+    public static Gasto CreateGasto(string titulogasto, string descricaogasto, string estabelecimento,
+        long gastodinheiro, DateTime datagasto)
+    {
+        Gasto gasto = new Gasto()
+        {
+            Id = Guid.NewGuid(),
+            TituloGasto = titulogasto,
+            DescricaoGasto = descricaogasto,
+            Estabelecimento = estabelecimento,
+            GastoDinheiro = gastodinheiro,
+            DataGasto = datagasto
+        };
+        return gasto;
+    }
 }
--- Domain/Entity/Financas/Receita.cs
+++ Domain/Entity/Financas/Receita.cs
@@ -1,6 +1,25 @@
 namespace Financas.Entity.Financas;
 
-public class Receita
+public class Receita : IEntity
 {
-    
+    public string TituloReceita { get; protected set; } = String.Empty;
+    public string? DescricaoReceita { get; protected set; }
+    public string? Estabelecimento { get; protected set; }
+    public long ReceitaDinheiro { get; protected set; }
+    public DateTime DataReceita { get; protected set; }
+
+    public static Receita CreateReceita(string tituloReceita, string descricaoReceita, string estabelecimento,
+        long Receitadinheiro, DateTime dataReceita)
+    {
+        Receita Receita = new Receita()
+        {
+            Id = Guid.NewGuid(),
+            TituloReceita = tituloReceita,
+            DescricaoReceita = descricaoReceita,
+            Estabelecimento = estabelecimento,
+            ReceitaDinheiro = Receitadinheiro,
+            DataReceita = dataReceita
+        };
+        return Receita;
+    }
 }
